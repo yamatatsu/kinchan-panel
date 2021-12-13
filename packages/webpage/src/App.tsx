2514bc78a7fe8b37a1eb08e0e4dc9186f7bcb341
yamatatsu <the.kinnikuman@gmail.com>
2021-12-13 23:01:55 +0900

    bump patch ver up

--- packages/webpage/src/App.tsx
+++ packages/webpage/src/App.tsx
@@ -1,5 +1,29 @@
 import { useEffect, useReducer, useState } from "react";
+import { API } from "aws-amplify";
 import { imgs, se } from "./constants";
+
+API.configure({
+  aws_project_region: "ap-northeast-1",
+  // aws_cognito_identity_pool_id: process.env.REACT_APP_IDENTITY_POOL_ID,
+  // aws_cognito_region: process.env.REACT_APP_REGION,
+  // aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
+  // aws_user_pools_web_client_id: process.env.REACT_APP_CLIENT_ID,
+  // oauth: {},
+  // Auth: {
+  //   // REQUIRED - Amazon Cognito Identity Pool ID
+  //   identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+  //   // REQUIRED - Amazon Cognito Region
+  //   region: process.env.REACT_APP_REGION,
+  //   // OPTIONAL - Amazon Cognito User Pool ID
+  //   userPoolId: process.env.REACT_APP_USER_POOL_ID,
+  //   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  //   userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,
+  // },
+  aws_appsync_graphqlEndpoint:
+    "https://xxxxxx.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-1",
+  aws_appsync_authenticationType: "API_KEY",
+});
 
 export default function App() {
   const [pm, serPm] = useState<PanelManager | null>(null);
@@ -136,7 +160,7 @@ export default function App() {
               <br />
               リセット：R キー
               <br />
-              v0.0.6
+              v0.0.7
             </div>
           </div>
         </div>
