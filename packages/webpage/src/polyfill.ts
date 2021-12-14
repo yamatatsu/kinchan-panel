06b945603ad92f02d54f346c56660ac75ea861a0
yamatatsu <the.kinnikuman@gmail.com>
2021-12-14 21:44:23 +0900

    env

---
+++ packages/webpage/src/polyfill.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+(window as any).global = window;
--- packages/webpage/src/vite-env.d.ts
+++ packages/webpage/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APPSYNC_GRAPHQL_ENDPOINT: string
+  readonly VITE_APPSYNC_REGION: string
+  readonly VITE_APPSYNC_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
